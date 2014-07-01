e8a9c4619e71c22f7240aa6262ee47b695842998
xinf <cuipaly009@163.com>
2014-07-01 12:14:45 +0800

    all change

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: './public/images' }));
 app.use(express.cookieParser());
 app.use(express.session({
   secret: settings.cookieSecret,
